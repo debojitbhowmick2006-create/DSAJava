72cdf65a22dc434caf0b2d8fead835b417422323
Debojit Bhowmick <debojitbhowmick2006@gmail.com>
2026-05-28 23:04:10 +0530

    commit

---
+++ src/PrettyPrinting.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+//       float a = 1234.32143f;
+//        System.out.printf("Formated number is %.2f", a);
+        System.out.printf("Pie is %.3f", Math.PI); //It also round off the value.
+        System.out.println();
+        System.out.printf("My name is %s and i am %s", "Debojit", "Student");
+        System.out.println();
+        System.out.println("a" + "b");
+        System.out.println('a' + 'b');
+        System.out.println((char)('a' + 3));
+        System.out.println((char)(100));
+        System.out.println("a" + 1); // integer will be converted to Integer that will call a toString()
+        System.out.println(new ArrayList<>());
+    }
+}
--- src/StringComparison.java
+++
@@ -1,6 +0,0 @@
-public class StringComparison {
-    public static void main(String[] args) {
-        String a = "Debojit";
-        System.out.println(a);
-    }
-}
